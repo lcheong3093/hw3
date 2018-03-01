b4111de0e73fe327e11ce56f4ea48f9bdfb9eafe
Calvin Li <calvin.li@stonybrook.edu>
2018-03-01 14:06:07 -0500

    winner

--- routes/index.js
+++ routes/index.js
@@ -205,8 +205,9 @@ router.post('/ttt/play', function(req, res) {
 							if (err) throw err;		
 							var ttt_db = db.db("ttt");
 							var myquery = { username:username } ;
+							var empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 							console.log("list: ", list);
-							var newvalues = { $set: { score: user.score, listgames:list } };	  
+							var newvalues = { $set: { score: user.score, listgames:list, grid: empty } };	  
 							ttt_db.collection("users").updateMany(myquery, newvalues, function(err, res) {
 								if (err) throw err;
 								console.log("updated score & listgames");
