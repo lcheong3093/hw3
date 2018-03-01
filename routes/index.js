90eb6fe81d2e0d634dc2a34d216829c5041873c5
Calvin Li <calvin.li@stonybrook.edu>
2018-03-01 00:12:29 -0500

    fixing for script

--- routes/index.js
+++ routes/index.js
@@ -146,8 +146,9 @@ router.post('/ttt/play', function(req, res) {
 	if(cookie === undefined){
 		console.log("no cookie from browser");
 	}else{
-		console.log(cookie.name);
+		console.log("cookie: ", cookie);
 	}
+	
 	var grid = req.body.grid;
 	var move = req.body.move;
 
