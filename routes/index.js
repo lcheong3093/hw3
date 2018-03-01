a89f9681d7d2bca74b2553f897ff658decf283f5
Calvin Li <calvin.li@stonybrook.edu>
2018-03-01 15:20:49 -0500

    winner

--- routes/index.js
+++ routes/index.js
@@ -267,20 +267,15 @@ router.post('/getgame', function(req, res) {
 	var cookie = req.cookies;
     var username = cookie.username;
 	var id = req.body.id;
-	console.log("**id: ", id);
 	mongoClient.connect(url, function(err, db) {
 		if (err) throw err;
 		var ttt_db = db.db("ttt");
 		ttt_db.collection("users").find({username: username}).toArray(function(err, item) {
 			if (err) throw err;
 			var user = item[0];
-			console.log("**user: ", user);
 			var allgames = user.games;
-			console.log("**allgames: ", allgames);
 			var game = allgames[id-1];
-			console.log("**game: ", game);
 			var data = {status: 'OK', grid: game.grid, winner: game.winner};
-			console.log("**data: ", data);
 			res.send(data);
 		});
 	});
@@ -289,25 +284,18 @@ router.post('/getgame', function(req, res) {
 router.post('/getscore', function(req, res) {
     var cookie = req.cookies;
     var username = cookie.username;
-    var user;
 	mongoClient.connect(url, function(err, db) {
 		if (err) throw err;
 		var ttt_db = db.db("ttt");
 		ttt_db.collection("users").find({username: username}).toArray(function(err, item) {
             if (err) throw err;
-			user = item[0];
-			if(user !== undefined){
-				console.log("user info:", user);
-				console.log("saved grid found: ", user.grid);
-			}else{
-				console.log("could not find saved game for: " + username);
-			}
+			var user = item[0];
+			var human = user.human;   
+			var wopr = user.wopr;
+			var tie = user.tie;
+			res.send({status: 'OK', human:human, wopr:wopr, tie:tie});		
 		});	
 	});
-    var human = user.human;   
-    var wopr = user.wopr;
-    var tie = user.tie;
-	res.send({status: 'OK', human:human, wopr:wopr, tie:tie});
 });
 
 function createMongoDB(){
