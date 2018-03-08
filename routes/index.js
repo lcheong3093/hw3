b810c07b59f6338ac2e657855a0d2043c92e7be2
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-03-08 02:50:01 -0500

    fix first arg

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,7 @@ router.post('/listen', function(req, res) {
 });
 
 router.post('/speak', function(req, res) {
-	console.log(req.msg.toString());
+	console.log(req.msg);
 	amqp.connect('amqp://localhost', function(err, conn) {
 		conn.createChannel(function(err, ch){
 			console.log("trying to publish msg");
