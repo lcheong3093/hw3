2a9860a99c57b39f605dbb54619257f21e1dace9
Lauren Cheong <lauren.cheong@stonybrook.edu>
2018-03-08 03:41:33 -0500

    closed connection

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.post('/listen', function(req, res) {
 				// console.log("send to queue");
 			});
 		});
-		// setTimeout(function() { conn.close()}, 500);
+		setTimeout(function() { conn.close()}, 500);
 	});
 
 });
